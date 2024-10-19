cb5f6e1c9ba929ed812b94b158607f697a7ef910
Brandon Tay <tayweiyong1007@gmail.com>
2024-10-20 00:51:59 +0800

    change database init

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,4 @@
-from .database_management import extract_file_info, get_multiple_info_by_id, get_info_by_id, write_info_to_file, relational_id_update
+from .database_management import extract_file_info, get_multiple_info_by_id, get_info_by_id, write_info_to_file, relational_id_update, update_info_by_id, insert_info, remove_by_id
 
-__all__ = ['extract_file_info', 'get_multiple_info_by_id', 'get_info_by_id', 'write_info_to_file', 'relational_id_update']
+__all__ = ['extract_file_info', 'get_multiple_info_by_id', 'get_info_by_id', 'write_info_to_file', 'relational_id_update', 'update_info_by_id', 'insert_info', 'remove_by_id']
 
